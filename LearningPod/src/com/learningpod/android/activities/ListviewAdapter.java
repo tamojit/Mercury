3e31d093bfd1274f94389428a291e3e8e59275c9
Deepak Pradeep <deepak@deepak-To-be-filled-by-O-E-M.(none)>
2014-05-19 22:09:12 +0530

    Change - Added wordlist activity

---
+++ LearningPod/src/com/learningpod/android/activities/ListviewAdapter.java
@@ -0,0 +1,99 @@
+package com.learningpod.android.activities;
+
+import static com.learningpod.android.activities.Constant.FIRST_COLUMN;
+import static com.learningpod.android.activities.Constant.SECOND_COLUMN;
+import static com.learningpod.android.activities.Constant.THIRD_COLUMN;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.learningpod.android.R;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class ListviewAdapter extends BaseAdapter
+{
+	public ArrayList<HashMap<String,String>> list;
+	Activity activity;
+	
+	public ListviewAdapter(Activity activity, ArrayList<HashMap<String,String>> list) {
+		super();
+		this.activity = activity;
+		this.list = list;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return list.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return list.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private class ViewHolder {
+		   LinearLayout layout;
+	       TextView txtFirst;
+	       TextView txtSecond;
+	       TextView txtThird;
+	      
+	  }
+
+	  
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		
+		// TODO Auto-generated method stub
+				ViewHolder holder;
+				LayoutInflater inflater =  activity.getLayoutInflater();
+
+				if (convertView == null)
+				{
+					convertView = inflater.inflate(R.layout.listview_row, null);
+					holder = new ViewHolder();
+					holder.txtFirst = (TextView) convertView.findViewById(R.id.FirstText);
+					holder.txtSecond = (TextView)convertView.findViewById(R.id.SecondText);
+					holder.txtThird = (TextView) convertView.findViewById(R.id.ThirdText);
+					holder.layout=(LinearLayout) convertView.findViewById(R.id.relativeLayout1);
+					
+					
+					convertView.setTag(holder);
+				}
+				else
+				{
+					holder = (ViewHolder) convertView.getTag();
+				}
+
+				HashMap<String, String> map = list.get(position);
+				holder.txtFirst.setText(map.get(FIRST_COLUMN));
+				holder.txtSecond.setText(map.get(SECOND_COLUMN));
+				holder.txtThird.setText(map.get(THIRD_COLUMN));
+			
+				if(position%2!=0)	{
+	                //   holder.layout.setBackgroundColor(Color.GRAY);
+					convertView.setBackgroundResource(R.drawable.choice_not_selected);
+	                                  }
+			
+			return convertView;
+     	}
+
+      }
---
+++ LearningPod/src/com/learningpod/android/activities/MultiColumnActivity.java
@@ -0,0 +1,163 @@
+package com.learningpod.android.activities;
+
+import static com.learningpod.android.activities.Constant.FIRST_COLUMN;
+import static com.learningpod.android.activities.Constant.SECOND_COLUMN;
+import static com.learningpod.android.activities.Constant.THIRD_COLUMN;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.NodeList;
+
+import com.learningpod.android.BaseActivity;
+import com.learningpod.android.R;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.provider.DocumentsContract.Document;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MultiColumnActivity extends BaseActivity 
+{
+	private ArrayList<HashMap<String,String>> list;
+	LinearLayout wordListContentView;
+	ListviewAdapter adapter;
+	//int noun[]={R.array.nounwordlist1,R.array.nounwordlist2};
+	int i=0;
+	int verb=0;
+	int vocabulary=0;
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        modifyActionBar();
+        wordListContentView=(LinearLayout)findViewById(R.id.listContentView);
+        createWordList();
+    }    
+   // String []description1={"Tier1","Tier2","Tier3"};
+    String []ListHeading = {"TIER I NOUNS","TIER II VERBS","TIER III VOCABULARY"};
+    
+	private void modifyActionBar() {
+		// get the action bar
+		ActionBar actionBar = getActionBar();
+		// getActionBar().setTitle(goToMapView.getText().toString());
+		getActionBar().setIcon(R.drawable.arrow);
+		actionBar.setCustomView(R.layout.custm);
+		TextView goToMapButton = (TextView) actionBar.getCustomView()
+				.findViewById(R.id.title);
+		TextView podTitle = (TextView) actionBar.getCustomView().findViewById(
+				R.id.podname);
+		podTitle.setText("WORDLIST");
+		podTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
+
+		//	podTitle.setTypeface(headerFont);
+		goToMapButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MultiColumnActivity.this,MapActivityBeforeLogin.class);
+				//intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+				//MultiColumnActivity.this.finish();
+			}
+		});
+
+		actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM
+				| ActionBar.DISPLAY_SHOW_HOME);
+
+	}
+   
+    private void createWordList() {
+    	for(int i=0;i<3;i++)
+    	{
+		LayoutInflater inflater=getLayoutInflater();
+		View wordListView=inflater.inflate(R.layout.word_list, null);
+		ListView wordList=(ListView)wordListView.findViewById(R.id.wordList);
+		
+		TextView ListHeadings=(TextView)wordListView.findViewById(R.id.wordListText);
+		ListHeadings.setText(ListHeading[i]);
+		
+		populateList(i);
+		adapter =new ListviewAdapter(MultiColumnActivity.this, list);
+		wordList.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, list.size()*62));
+		wordList.setAdapter(adapter);
+		wordListContentView.addView(wordListView);
+    	}
+	}
+
+	private void populateList(int k) {
+    	
+		switch(k){
+		
+		case 0:
+	
+         	list = new ArrayList<HashMap<String,String>>();
+    		Resources res = getResources();
+			String []nounitems = res.getStringArray(R.array.nounwordlist);
+    		int j= nounitems.length;   
+    	    while(i<j){
+		    HashMap<String,String> temp = new HashMap<String,String>();
+			temp.put(FIRST_COLUMN,nounitems[i]);
+			temp.put(SECOND_COLUMN,nounitems[i+1]);
+			temp.put(THIRD_COLUMN, nounitems[i+2]);
+			i=i+3;
+	      	list.add(temp);
+    		}
+    	          break;
+    	
+    	case 1:
+    	
+    		list = new ArrayList<HashMap<String,String>>();
+    		Resources res1 = getResources();
+			String []verbitems = res1.getStringArray(R.array.verbwordlist);
+    		int z= verbitems.length;   
+    	    while(verb<z){
+		    HashMap<String,String> temp = new HashMap<String,String>();
+			temp.put(FIRST_COLUMN,verbitems[verb]);
+			temp.put(SECOND_COLUMN,verbitems[verb+1]);
+			temp.put(THIRD_COLUMN, verbitems[verb+2]);
+			verb=verb+3;
+	      	list.add(temp);
+    		}
+    	          break;
+		
+    	
+    	
+    	case 2:
+        	
+    		list = new ArrayList<HashMap<String,String>>();
+    		Resources res2 = getResources();
+			String []vocabularyitems = res2.getStringArray(R.array.vocabularywordlist);
+    		int h= vocabularyitems.length;   
+    	    while(vocabulary<h){
+		    HashMap<String,String> temp = new HashMap<String,String>();
+			temp.put(FIRST_COLUMN,vocabularyitems[vocabulary]);
+			temp.put(SECOND_COLUMN,vocabularyitems[vocabulary+1]);
+			temp.put(THIRD_COLUMN, vocabularyitems[vocabulary+2]);
+			vocabulary=vocabulary+3;
+	      	list.add(temp);
+    		}
+    	          break;
+    	
+		}
+	}
+}
